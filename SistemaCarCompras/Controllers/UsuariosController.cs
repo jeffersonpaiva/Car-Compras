366cef0a393433632f2565e9e08006620a87a1f6
Jefferson <jeffersondesouzapaiva@hotmail.com>
2019-06-24 15:45:35 -0300

    Iniciando a criar o login

---
+++ SistemaCarCompras/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaCarCompras.Models;
+
+namespace SistemaCarCompras.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Verificar(Usuario usu)
+        {
+            return View();
+        }
+    }
+}
