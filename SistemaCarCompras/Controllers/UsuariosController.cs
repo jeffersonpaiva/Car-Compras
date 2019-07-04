4b1a8283221b876a1edcb25b7337b8bdc1b10565
Jefferson <jeffersondesouzapaiva@hotmail.com>
2019-07-04 10:57:51 -0300

    Conlcuindo o usuario

--- SistemaCarCompras/Controllers/UsuariosController.cs
+++ SistemaCarCompras/Controllers/UsuariosController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SistemaCarCompras.Models;
+using System.Data.SqlClient;
 
 namespace SistemaCarCompras.Controllers
 {
     public class UsuariosController : Controller
     {
+        SqlCommand con = new SqlCommand();
+        private SqlConnection _con;
+        SqlDataReader dr;
         // GET: Usuarios
         [HttpGet]
         public ActionResult Login()
@@ -16,9 +20,27 @@ namespace SistemaCarCompras.Controllers
             return View();
         }
 
+
+
+
         public ActionResult Verificar(Usuario usu)
         {
-            return View();
+            _con.Open();
+            con.CommandText = "select * from Usuarios where usuario = '"+usu.Nome+"' and Senha = '"+usu.Senha+"'";
+            dr = con.ExecuteReader();
+
+            if (dr.Read())
+            {
+                _con.Close();
+                return View();
+            }
+            else
+            {
+                _con.Close();
+                return View();
+            }
+
+            
         }
     }
 }
